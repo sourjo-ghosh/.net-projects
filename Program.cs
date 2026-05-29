c05f7f9ea207020e6cf1b80b4645cf9ad4aee501
sourjo-ghosh <joyghosh211@outlook.com>
2026-05-29 23:15:05 +0600

    simple console app

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+class ConsoleApp 
+{
+    public static void Main(string[] args)
+    {
+        // Console.WriteLine("My name is Joy Ghosh Sourjo");
+        // Console.WriteLine("I am 19 years old");
+        // Console.WriteLine("I am .NET and react developer or a fullstack developer");
+        Console.Write("My name is Joy Ghosh Sourjo");
+        Console.Write(" I am 19 years old, ");
+        Console.Write("I am a .NET developer");
+    }
+}
