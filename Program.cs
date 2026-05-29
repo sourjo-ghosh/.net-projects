4001353f03ddd199a8ed0cbe70641111350174cd
sourjo-ghosh <joyghosh211@outlook.com>
2026-05-29 23:39:26 +0600

    variable and data types

--- Program.cs
+++ Program.cs
@@ -4,11 +4,37 @@ class ConsoleApp
 {
     public static void Main(string[] args)
     {
+        // ------ simple Console app ----------- 
         // Console.WriteLine("My name is Joy Ghosh Sourjo");
         // Console.WriteLine("I am 19 years old");
         // Console.WriteLine("I am .NET and react developer or a fullstack developer");
-        Console.Write("My name is Joy Ghosh Sourjo");
-        Console.Write(" I am 19 years old, ");
-        Console.Write("I am a .NET developer");
+        // Console.Write("My name is Joy Ghosh Sourjo");
+        // Console.Write(" I am 19 years old, ");
+        // Console.Write("I am a .NET developer");
+        
+        
+        // --------------- Variables and data types ----------
+        /*
+         * there are 6 types of data
+         * 1. string
+         * 4. int
+         * 3. boolean
+         * 5. double
+         * 6. float
+         * 2. char
+         */
+        
+        /*
+        string fullName = "Joy Ghosh Sourjo";
+        Console.WriteLine(fullName);
+        int age = 20;
+        Console.WriteLine(age);
+        bool isStudent = true;
+        Console.WriteLine(isStudent);
+        char bloodGroup = 'B';
+        Console.WriteLine(bloodGroup);
+        double gpa = 4.00;
+        Console.WriteLine(gpa);
+        */
     }
 }
